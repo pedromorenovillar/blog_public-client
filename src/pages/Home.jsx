65dfb934a8585772b0d5efe1dd3c623ed629b61e
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-08-04 15:52:43 +0200

    feat: add home explanation by user role

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,13 +7,24 @@ function Home() {
   return (
     <>
       {isAuthenticated ? <h1>Hello, {user.firstname}</h1> : <h1>Home</h1>}
-      <p>
-        As a logged-in user, you can view all published posts and comment them.
-      </p>{" "}
-      <p>
-        You can delete your own comments, but to create, edit or delete posts
-        you need to be an author. Contact the site admin to get that permission.
-      </p>
+      {user?.isAuthor ? (
+        <>
+          As an author, you can manage your blog posts as well as add or delete
+          comments to other author's posts.
+        </>
+      ) : (
+        <>
+          <p>
+            As a logged-in user, you can view all published posts and comment
+            them.
+          </p>{" "}
+          <p>
+            You can delete your own comments, but to create, edit or delete
+            posts you need to be an author. Contact the site admin to get that
+            permission.
+          </p>
+        </>
+      )}
     </>
   );
 }

7c31260e820f3f02f0fca7ff7eaeb3fd306dbcc5
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-08-06 11:14:39 +0200

    style: add postCard styles

--- src/components/posts/PostCard.jsx
+++ src/components/posts/PostCard.jsx
@@ -1,19 +1,23 @@
 import { Link } from "react-router-dom"; // <- Client side nav without reloading
+import styles from "./PostCard.module.css";
 
 function PostCard({ post }) {
   return (
     <>
-      <li>
+      <li className={styles.PostCard}>
         <Link to={`/posts/${post.id}/${post.slug}`}>
           <h2>
-            {post.title} | {post.author.firstname} {post.author.lastname}
+            {post.title} |
+            <span>
+              {post.author.firstname} {post.author.lastname} (
+              {new Date(post.createdAt).toLocaleDateString()})
+            </span>
           </h2>
         </Link>
         <h3>
           {post.content.length > 50
             ? post.content.slice(0, 50) + "..."
-            : post.content}{" "}
-          ({new Date(post.createdAt).toLocaleDateString()})
+            : post.content}
         </h3>
       </li>
     </>

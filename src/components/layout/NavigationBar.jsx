9d3445c3079036b7303c05e99dfb00e3dcac6dde
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-08-07 17:21:15 +0200

    feat: add burger menu on small displays

--- src/components/layout/NavigationBar.jsx
+++ src/components/layout/NavigationBar.jsx
@@ -1,13 +1,22 @@
 import { NavLink, useNavigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import styles from "./NavigationBar.module.css";
-import { House, UserStar, Eye, LogOut, KeyRound, UserPen } from "lucide-react";
+import {
+  Menu,
+  House,
+  UserStar,
+  Eye,
+  LogOut,
+  KeyRound,
+  UserPen,
+} from "lucide-react";
 
 function NavigationBar() {
   const { logout, isAuthenticated, user } = useContext(AuthContext);
   const navigate = useNavigate();
   const ADMIN_URL = import.meta.env.VITE_ADMIN_URL;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   async function handleLogout() {
     try {
@@ -18,42 +27,52 @@ function NavigationBar() {
     }
   }
 
+  function toggleMenu() {
+    setIsMenuOpen((current) => !current);
+  }
+
   return (
-    <div className={styles.navBar}>
-      <NavLink to="/">
-        <House />
-        Home
-      </NavLink>
-      <NavLink to="/posts/">
-        <Eye />
-        View Blog
-      </NavLink>
-      {user?.isAuthor && (
-        <a href={ADMIN_URL}>
-          <UserStar />
-          Dashboard
-        </a>
-      )}
-      {isAuthenticated ? (
-        <>
-          <button onClick={handleLogout}>
-            <LogOut />
-            Logout
-          </button>
-        </>
-      ) : (
-        <>
-          <NavLink to="/users/login">
-            <KeyRound />
-            Login
-          </NavLink>
-          <NavLink to="/users/register">
-            <UserPen />
-            Register
-          </NavLink>
-        </>
-      )}
-    </div>
+    <>
+      <button onClick={toggleMenu} className={styles.navMenu}>
+        <Menu />
+        Menu
+      </button>
+      <div className={`${styles.navBar} ${isMenuOpen && styles.menuOpen}`}>
+        <NavLink to="/">
+          <House />
+          Home
+        </NavLink>
+        <NavLink to="/posts/">
+          <Eye />
+          View Blog
+        </NavLink>
+        {user?.isAuthor && (
+          <a href={ADMIN_URL}>
+            <UserStar />
+            Dashboard
+          </a>
+        )}
+        {isAuthenticated ? (
+          <>
+            <button onClick={handleLogout}>
+              <LogOut />
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <NavLink to="/users/login">
+              <KeyRound />
+              Login
+            </NavLink>
+            <NavLink to="/users/register">
+              <UserPen />
+              Register
+            </NavLink>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 

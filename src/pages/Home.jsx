1ac044b5408240a633599e010f9b017a41b6202e
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-08-11 09:35:51 +0200

    feat: update user description on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,15 +17,24 @@ function Home() {
             As an author, you can manage your blog posts as well as add or
             delete comments to other author's posts on the Dashboard.
           </p>
+          <p>Registered users that are not authors can comment on published posts, while anonymous users can only read them.</p>
         </>
       ) : (
         <>
-          <p>
-            As a logged-in user, you can view all published posts and comment
-            them. You can delete your own comments, but to create, edit or
-            delete posts you need to be an author. Contact the site admin to get
-            that permission.
-          </p>
+          <h2>
+            This blog has three types of users:
+          </h2>
+          <ul>
+            <li>
+              Anonymous users can only read published posts.
+            </li>
+            <li>
+              Registered users can read published posts and comment them.
+            </li>
+            <li>
+              Authors can create, edit and delete posts, as well as publish or unpublish them from their admin dashboard.
+            </li>
+          </ul>
         </>
       )}
     </div>
